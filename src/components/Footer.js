4e42438c60a5beb74284d71ac74dfe40b9f6b6f2
Lawrence Eadie <lawrence@connectthedotsclub.com>
2023-04-17 15:08:44 -0400

    Background and footer updates

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,15 +6,25 @@ import './Footer.css';
 function Footer() {
   return (
     <footer className="footer">
+      <div className="footer-links">
+        <Link className="footer-link" to="/cookie-policy">
+          Cookie Policy
+        </Link>
+        <span className="footer-link">|</span>
+        <Link className="footer-link" to="/terms-of-service">
+          Terms of Service
+        </Link>
+        <span className="footer-link">|</span>
+        <Link className="footer-link" to="/privacy-policy">
+          Privacy Policy
+        </Link>
+      </div>
       <p>
-        &copy; {new Date().getFullYear()} Connect the Dots Club. All rights reserved.{' '}
-        <Link to="/cookie-policy">Cookie Policy</Link> |{' '}
-        <Link to="/terms-of-service">Terms of Service</Link> |{' '}
-        <Link to="/privacy-policy">Privacy Policy</Link>
+        &copy; {new Date().getFullYear()} Connect the Dots Club. All rights
+        reserved.{' '}
       </p>
     </footer>
   );
 }
 
 export default Footer;
-
